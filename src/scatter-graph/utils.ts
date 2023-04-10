eeb733ddb6168223236f7a2b0b8b69dd6048656e
Austin <austin@keyvalue.systems>
2023-04-10 11:28:28 +0530

    Minor change

--- src/scatter-graph/utils.ts
+++ src/scatter-graph/utils.ts
@@ -1,8 +1,14 @@
 import { AxisRanges, AxisRangesArguments } from "../types/types";
 
+// This function takes in a range of values and calculates a suitable interval to
+// divide that range into smaller segments for display purposes.
 const getInterval = (min: number, max: number): number => {
   const range = max - min;
+  // This formula calculates an interval that is a power of 10,
+  //  such as 1, 10, 100, etc., based on the magnitude of the range.
   let interval = Math.pow(10, Math.floor(Math.log10(range)));
+  // The array [1, 2, 5] represents a common set
+  // of "step" values used to create commonly-used and visually appealing intervals on a graph or chart.
   const steps = [1, 2, 5];
   for (let i = 0; i < steps.length; i++) {
     if ((range / (interval * steps[i])) <= 8) {
@@ -25,6 +31,7 @@ export const getAxisRanges = (testData: AxisRangesArguments): AxisRanges => {
     if (y > ymax) ymax = y;
   }
 
+  // calculate a suitable interval for a range of values between min and max.
   const xInterval = getInterval(xmin, xmax);
   const yInterval = getInterval(ymin, ymax);
 
